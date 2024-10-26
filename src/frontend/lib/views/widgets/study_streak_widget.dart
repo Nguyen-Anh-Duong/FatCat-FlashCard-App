774cecc1fa54ba1da33a906c77952ad81c5fd19a
madmoon <madmoontokyo@gmail.com>
2024-10-26 14:40:56 +0700

    add user file

--- src/frontend/lib/views/widgets/study_streak_widget.dart
+++ src/frontend/lib/views/widgets/study_streak_widget.dart
@@ -1,6 +1,6 @@
 import 'package:FatCat/constants/colors.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
+// import 'package:intl/intl.dart';
 
 class StudyStreakWidget extends StatelessWidget {
   final int streak;
