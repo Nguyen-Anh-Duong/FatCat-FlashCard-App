cd58a383516756cf6f65bccbbdc802b77282cecf
naDuong2004 <nguyenanhduong112004@gmail.com>
2024-10-27 09:31:23 +0700

    test

--- src/backend/src/app.js
+++ src/backend/src/app.js
@@ -18,22 +18,22 @@ require("./database/init.database");
 //init routes
 app.use("/", require("./routes"));
 
-
 //handle errors
 app.use((req, res, next) => {
-    const error = new Error("Not found");
-    error.status = 404;
-    next(error);
+  const error = new Error("Not found");
+  error.status = 404;
+  next(error);
 });
 
 app.use((error, req, res, next) => {
-    res.status(error.status || 500);
-    res.json({
-        status: error.status,
-        code: error.code,
-        message: error.message || "Internal Server Error",
-        stack: error.stack,
-    })
-})
+  console.log(hehe);
+  res.status(error.status || 500);
+  res.json({
+    status: error.status,
+    code: error.code,
+    message: error.message || "Internal Server Error",
+    stack: error.stack,
+  });
+});
 
 module.exports = app;
