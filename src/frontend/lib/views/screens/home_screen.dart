846907ab96023935f975477362ea4c1bea727a7e
DUYCUTEPHOMAIQUE <dauhu1232019@gmail.com>
2024-10-28 11:31:26 +0700

    push lai thanh thong bao mau den

--- src/frontend/lib/views/screens/home_screen.dart
+++ src/frontend/lib/views/screens/home_screen.dart
@@ -10,133 +10,143 @@ import 'package:FatCat/views/widgets/folder_deck_list_widget.dart';
 import 'package:FatCat/views/widgets/study_streak_widget.dart';
 import 'package:FatCat/views/widgets/text_and_showall_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
+    //mau cho thanh thong bao
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      statusBarColor: Colors.black,
+      statusBarIconBrightness: Brightness.light,
+    ));
+
     return Scaffold(
       backgroundColor: Colors.white,
-      body: SingleChildScrollView(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              ClipPath(
-                clipper: CurvedClipper(),
-                child: Container(
-                  color: AppColors.backgroundButtonColor,
-                  height: 160.0,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Center(
-                        child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            const Padding(
-                              padding: EdgeInsets.only(left: 8.0),
-                              child: Text(
-                                'Fat Cat',
-                                style: TextStyle(
-                                    color: AppColors.white,
-                                    fontSize: 28,
-                                    fontWeight: FontWeight.bold),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                ClipPath(
+                  clipper: CurvedClipper(),
+                  child: Container(
+                    color: AppColors.backgroundButtonColor,
+                    height: 160.0,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Center(
+                          child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              const Padding(
+                                padding: EdgeInsets.only(left: 8.0),
+                                child: Text(
+                                  'Fat Cat',
+                                  style: TextStyle(
+                                      color: AppColors.white,
+                                      fontSize: 28,
+                                      fontWeight: FontWeight.bold),
+                                ),
                               ),
-                            ),
-                            IconButton(
-                              icon: const Icon(
-                                Icons.notifications,
-                                size: 32,
-                                color: AppColors.white,
+                              IconButton(
+                                icon: const Icon(
+                                  Icons.notifications,
+                                  size: 32,
+                                  color: AppColors.white,
+                                ),
+                                onPressed: () {
+                                  //todo open notification dialog
+                                },
                               ),
-                              onPressed: () {
-                                //todo open notification dialog
-                              },
-                            ),
-                          ],
-                        )
-                      ],
-                    )),
-                  ),
-                ),
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  const TextAndShowAllWidget(text: 'Thư mục'),
-                  SizedBox(
-                    height: 150,
-                    child: ListView.builder(
-                      scrollDirection: Axis.horizontal,
-                      itemCount: 10,
-                      itemBuilder: (context, index) {
-                        return const DeckHomeItemWidget(
-                            name: "English",
-                            description: 'Tieng Anh',
-                            userCreate: 'Admin');
-                      },
+                            ],
+                          )
+                        ],
+                      )),
                     ),
                   ),
-                ],
-              ),
-              const SizedBox(
-                height: 8,
-              ),
-              const TextAndShowAllWidget(text: 'Streak', isShowAll: false),
-              Padding(
-                padding: const EdgeInsets.only(left: 16, right: 16),
-                child: StudyStreakWidget(
-                    streak: 3,
-                    streakStartAt: DateTime.now().add(const Duration(days: 2))),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              const TextAndShowAllWidget(
-                  text: 'Duyệt theo chủ đề', isShowAll: false),
-              GridView.builder(
-                padding: const EdgeInsets.only(left: 8, right: 8),
-                shrinkWrap: true,
-                physics: const NeverScrollableScrollPhysics(),
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 3,
-                  mainAxisSpacing: 10,
-                  crossAxisSpacing: 10,
-                  childAspectRatio: 0.8,
                 ),
-                itemCount: 6,
-                itemBuilder: (context, index) {
-                  final List<String> icons = [
-                    'assets/icons/language.png',
-                    'assets/icons/math.png',
-                    'assets/icons/science.png',
-                    'assets/icons/art.png',
-                    'assets/icons/literary.png',
-                    'assets/icons/unknown.png',
-                  ];
-                  final List<String> labels = [
-                    'Ngôn ngữ',
-                    'Toán học',
-                    'Khoa học',
-                    'Nghệ thuật',
-                    'Văn học',
-                    'Khác',
-                  ];
-                  return Column(
-                    children: [
-                      CategoryHomeWidget(iconPath: icons[index]),
-                      Text(labels[index],
-                          style: AppTextStyles.categoryHomeText),
-                    ],
-                  );
-                },
-              ),
-            ],
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    const TextAndShowAllWidget(text: 'Thư mục'),
+                    SizedBox(
+                      height: 150,
+                      child: ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemCount: 10,
+                        itemBuilder: (context, index) {
+                          return const DeckHomeItemWidget(
+                              name: "English",
+                              description: 'Tieng Anh',
+                              userCreate: 'Admin');
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                const TextAndShowAllWidget(text: 'Streak', isShowAll: false),
+                Padding(
+                  padding: const EdgeInsets.only(left: 16, right: 16),
+                  child: StudyStreakWidget(
+                      streak: 3,
+                      streakStartAt:
+                          DateTime.now().add(const Duration(days: 2))),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const TextAndShowAllWidget(
+                    text: 'Duyệt theo chủ đề', isShowAll: false),
+                GridView.builder(
+                  padding: const EdgeInsets.only(left: 8, right: 8),
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    mainAxisSpacing: 10,
+                    crossAxisSpacing: 10,
+                    childAspectRatio: 0.8,
+                  ),
+                  itemCount: 6,
+                  itemBuilder: (context, index) {
+                    final List<String> icons = [
+                      'assets/icons/language.png',
+                      'assets/icons/math.png',
+                      'assets/icons/science.png',
+                      'assets/icons/art.png',
+                      'assets/icons/literary.png',
+                      'assets/icons/unknown.png',
+                    ];
+                    final List<String> labels = [
+                      'Ngôn ngữ',
+                      'Toán học',
+                      'Khoa học',
+                      'Nghệ thuật',
+                      'Văn học',
+                      'Khác',
+                    ];
+                    return Column(
+                      children: [
+                        CategoryHomeWidget(iconPath: icons[index]),
+                        Text(labels[index],
+                            style: AppTextStyles.categoryHomeText),
+                      ],
+                    );
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),
