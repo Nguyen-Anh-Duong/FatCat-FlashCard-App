300adc8f69064d0021f215f5bc24051e23bbb295
DUYCUTEPHOMAIQUE <dauhu1232019@gmail.com>
2024-11-13 17:23:01 +0700

    be class cho dong bo

--- src/frontend/lib/viewmodels/class_viewmodel.dart
+++ src/frontend/lib/viewmodels/class_viewmodel.dart
@@ -30,6 +30,7 @@ class ClassViewModel extends ChangeNotifier {
   Future<void> fetchOwnClasses() async {
     try {
       _isLoading = true;
+      await Future.delayed(const Duration(seconds: 1));
       notifyListeners();
 
       _ownClasses = await _classService.getOwnClasses();
