1b271885f1e3f36742a8399ec5728085413c02ca
DUYCUTEPHOMAIQUE <dauhu1232019@gmail.com>
2024-11-14 03:03:33 +0700

    OKK

--- src/backend/src/app.js
+++ src/backend/src/app.js
@@ -25,9 +25,9 @@ const app = express();
 app.use(morgan("dev"));
 app.use(helmet());
 app.use(compression());
-app.use(express.json());
 app.use(cors());
 app.use(express.json({ charset: 'utf-8' }));
+app.use(express.urlencoded({ extended: true, charset: 'utf-8' }));
 
 //init database
 
